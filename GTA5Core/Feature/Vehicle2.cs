f4c94b03cabc3b8929ae57b21c9dee84c3f45f5a
穗乃果 <1763575579@qq.com>
2023-02-24 07:56:24 +0800

    fix bug

--- GTA5Core/Feature/Vehicle2.cs
+++ GTA5Core/Feature/Vehicle2.cs
@@ -36,50 +36,57 @@ public static class Vehicle2
             else
                 vector3.Z += z255;
 
-            Hacks.WriteGA(Offsets.oVMCreate + 7 + 0, vector3.X);        // 载具坐标x
-            Hacks.WriteGA(Offsets.oVMCreate + 7 + 1, vector3.Y);        // 载具坐标y
-            Hacks.WriteGA(Offsets.oVMCreate + 7 + 2, vector3.Z);        // 载具坐标z
-
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 66, Hacks.Joaat(model));     // 载具哈希值
+
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 95, 14);     // Ownerflag  拥有者标志
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 94, 2);      // Personal car ownerflag  个人载具拥有者标志
 
             Hacks.WriteGA(Offsets.oVMCreate + 3, 0);        // pegasus
             Hacks.WriteGA(Offsets.oVMCreate + 5, 1);        // can spawn flag must be odd
             Hacks.WriteGA(Offsets.oVMCreate + 2, 1);        // spawn toggle gets reset to 0 on car spawn
 
+            Hacks.WriteGA(Offsets.oVMCreate + 7 + 0, vector3.X);        // 载具坐标x
+            Hacks.WriteGA(Offsets.oVMCreate + 7 + 1, vector3.Y);        // 载具坐标y
+            Hacks.WriteGA(Offsets.oVMCreate + 7 + 2, vector3.Z);        // 载具坐标z
+
             Hacks.WriteGAString(Offsets.oVMCreate + 27 + 1, Guid.NewGuid().ToString()[..8]);    // License plate  车牌
 
-            // 最大化升级载具
-            for (int i = 0; i < 50; i++)
+            // 这两辆载具使用mod会导致崩溃
+            if (model != "issi8" && model != "entity3")
             {
-                if (i < 17)
+                // 最大化升级载具
+                for (int i = 0; i < 50; i++)
                 {
-                    Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + i, mod[i]);
-                }
-                else if (i > 22 && i < 48)
-                {
-                    Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + 6 + i, mod[i]);
-                }
-                else if (i == 48 && mod[48] > 0)
-                {
-                    // 随机涂装
-                    Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + 48, new Random().Next(0, mod[42] + 1));
+                    if (i < 17)
+                    {
+                        Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + i, mod[i]);
+                    }
+                    else if (i > 22 && i < 48)
+                    {
+                        Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + 6 + i, mod[i]);
+                    }
+                    else if (i == 48 && mod[48] > 0)
+                    {
+                        // 随机涂装
+                        Hacks.WriteGA(Offsets.oVMCreate + 27 + 10 + 48, new Random().Next(0, mod[48] + 1));
+                    }
                 }
             }
 
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 5, new Random().Next(0, 160));      // primary -1 auto 159  主色调
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 6, new Random().Next(0, 160));      // secondary -1 auto 159  副色调
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 7, new Random().Next(0, 160));      // pearlescent  珠光色
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 5, -1);      // primary -1 auto 159  主色调
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 6, -1);      // secondary -1 auto 159  副色调
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 7, -1);      // pearlescent  珠光色
 
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 8, -1);      // wheel color  车轮颜色
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 69, -1);     // Wheel type  车轮类型
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 33, -1);     // wheel selection  车轮选择
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 8, new Random().Next(0, 160));      // wheel color  车轮颜色
 
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 24, new Random().Next(0, mod[14] + 1));      // 喇叭
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 27, 1);      // Turbo (0-1)  涡轮增压
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 28, 1);      // weaponised ownerflag
 
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 30, 1);      // 烧胎烟雾
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 32, new Random().Next(0, 15));      // 氙气车灯 (0-14)
+            Hacks.WriteGA(Offsets.oVMCreate + 27 + 32, 1);      // 氙气车灯 (0-14)
 
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 60, 1);      // landinggear/vehstate  起落架/载具状态
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 62, new Random().Next(0, 256));      // Tire smoke color R  烧胎烟雾颜色
@@ -92,9 +99,6 @@ public static class Vehicle2
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 76, new Random().Next(0, 256));      // B
 
             Hacks.WriteGA(Offsets.oVMCreate + 27 + 77, 0xF0400200);   // vehstate 4030726305  载具状态 没有这个载具起落架是收起状态
-
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 95, 14);     // Ownerflag  拥有者标志
-            Hacks.WriteGA(Offsets.oVMCreate + 27 + 94, 2);      // Personal car ownerflag  个人载具拥有者标志
 
         });
     }

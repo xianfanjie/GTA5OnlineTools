cbb93d01dd514c94a1228e40b9f1f50303339758
穗乃果 <1763575579@qq.com>
2023-02-24 00:25:56 +0800

    update

--- GTA5OnlineTools/Properties/AssemblyInfo.cs
+++ GTA5OnlineTools/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@
 [assembly: AssemblyTitle("GTA5线上小助手")]
 [assembly: AssemblyCompany("CrazyZhang")]
 [assembly: AssemblyProduct("GTA5线上小助手")]
-[assembly: AssemblyVersion("3.0.9.8")]
-[assembly: AssemblyFileVersion("3.0.9.8")]
+[assembly: AssemblyVersion("3.0.9.9")]
+[assembly: AssemblyFileVersion("3.0.9.9")]
 
 // 不重复
 [assembly: AssemblyDescription("https://github.com/CrazyZhang666/GTA5OnlineTools")]

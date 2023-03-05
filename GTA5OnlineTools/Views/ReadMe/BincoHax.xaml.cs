91dcff6634dc95a293b4aa9862426c33964d48a1
穗乃果 <1763575579@qq.com>
2023-03-05 13:33:47 +0800

    update

--- GTA5OnlineTools/Views/ReadMe/BincoHax.xaml.cs
+++ GTA5OnlineTools/Views/ReadMe/BincoHax.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace GTA5OnlineTools.Views.ReadMe;
+﻿using GTA5Shared.Helper;
+
+namespace GTA5OnlineTools.Views.ReadMe;
 
 /// <summary>
 /// BincoHax.xaml 的交互逻辑
@@ -8,5 +10,16 @@ public partial class BincoHax : UserControl
     public BincoHax()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// 超链接请求导航事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        ProcessHelper.OpenLink(e.Uri.OriginalString);
+        e.Handled = true;
     }
 }
--- GTA5OnlineTools/Views/ReadMe/YimMenu.xaml.cs
+++ GTA5OnlineTools/Views/ReadMe/YimMenu.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace GTA5OnlineTools.Views.ReadMe;
+﻿using GTA5Shared.Helper;
+
+namespace GTA5OnlineTools.Views.ReadMe;
 
 /// <summary>
 /// YimMenu.xaml 的交互逻辑
@@ -8,5 +10,16 @@ public partial class YimMenu : UserControl
     public YimMenu()
     {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// 超链接请求导航事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        ProcessHelper.OpenLink(e.Uri.OriginalString);
+        e.Handled = true;
     }
 }

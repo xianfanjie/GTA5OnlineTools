61a977bb59dc3f868fff881e04415e6390e8b54d
穗乃果 <1763575579@qq.com>
2023-07-06 18:35:36 +0800

    修复幸运转盘失效bug

--- GTA5MenuExtra/CasinoHackWindow.xaml.cs
+++ GTA5MenuExtra/CasinoHackWindow.xaml.cs
@@ -9,6 +9,11 @@ namespace GTA5MenuExtra;
 /// </summary>
 public partial class CasinoHackWindow
 {
+    private bool _disposed = false;
+
+    private int luckyWheelSlot = -1;
+    private int slotMachineSlot = -1;
+
     public CasinoHackWindow()
     {
         InitializeComponent();
@@ -16,46 +21,62 @@ public partial class CasinoHackWindow
 
     private void Window_CasinoHack_Loaded(object sender, RoutedEventArgs e)
     {
-
+        new Thread(CasinoHackLoopThread)
+        {
+            Name = "CasinoHackLoopThread",
+            IsBackground = true
+        }.Start();
     }
 
     private void Window_CasinoHack_Closing(object sender, CancelEventArgs e)
     {
-        
+
+    }
+
+    private void CasinoHackLoopThread()
+    {
+        while (!_disposed)
+        {
+            // 幸运轮盘
+            if (luckyWheelSlot != -1)
+            {
+                Locals.WriteLocalAddress("casino_lucky_wheel", 276 + 14, luckyWheelSlot);
+            }
+
+            // 老虎机
+            if (slotMachineSlot != -1)
+            {
+                var pointer = Locals.LocalAddress("casino_slots");
+                if (Memory.IsValid(pointer))
+                {
+                    pointer = Memory.Read<long>(pointer);
+
+                    for (var i = 0; i < 3; i++)
+                    {
+                        for (var j = 0; j < 64; j++)
+                        {
+                            var offset = 1344 + 1 + 1 + i * 65 + 1 + j;
+                            Memory.Write(pointer + offset * 8, slotMachineSlot);
+                        }
+                    }
+                }
+            }
+
+            Thread.Sleep(250);
+        }
     }
 
     private void Button_SetLuckyWheelSlot_Click(object sender, RoutedEventArgs e)
     {
         AudioHelper.PlayClickSound();
 
-        var index = ListBox_LuckyWheel.SelectedIndex;
-        if (index == -1)
-            return;
-
-        Locals.WriteLocalAddress("casino_lucky_wheel", 276 + 14, index);
+        luckyWheelSlot = ListBox_LuckyWheel.SelectedIndex;
     }
 
     private void Button_SlotMachineSlot_Click(object sender, RoutedEventArgs e)
     {
         AudioHelper.PlayClickSound();
 
-        var index = ListBox_SlotMachine.SelectedIndex;
-        if (index == -1)
-            return;
-
-        var pointer = Locals.LocalAddress("casino_slots");
-        if (Memory.IsValid(pointer))
-        {
-            pointer = Memory.Read<long>(pointer);
-
-            for (var i = 0; i < 3; i++)
-            {
-                for (var j = 0; j < 64; j++)
-                {
-                    var offset = 1344 + 1 + 1 + i * 65 + 1 + j;
-                    Memory.Write(pointer + offset * 8, index);
-                }
-            }
-        }
+        slotMachineSlot = ListBox_SlotMachine.SelectedIndex;
     }
 }

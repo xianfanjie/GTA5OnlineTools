428fcaa1edcdcd712aa28290421e151a533a8f28
穗乃果 <1763575579@qq.com>
2023-07-08 02:31:45 +0800

    修复幸运轮盘bug

--- GTA5MenuExtra/CasinoHackWindow.xaml.cs
+++ GTA5MenuExtra/CasinoHackWindow.xaml.cs
@@ -12,7 +12,6 @@ public partial class CasinoHackWindow
     private bool _disposed = false;
 
     private int luckyWheelSlot = -1;
-    private int slotMachineSlot = -1;
 
     public CasinoHackWindow()
     {
@@ -43,26 +42,7 @@ public partial class CasinoHackWindow
                 Locals.WriteLocalAddress("casino_lucky_wheel", 276 + 14, luckyWheelSlot);
             }
 
-            // 老虎机
-            if (slotMachineSlot != -1)
-            {
-                var pointer = Locals.LocalAddress("casino_slots");
-                if (Memory.IsValid(pointer))
-                {
-                    pointer = Memory.Read<long>(pointer);
-
-                    for (var i = 0; i < 3; i++)
-                    {
-                        for (var j = 0; j < 64; j++)
-                        {
-                            var offset = 1344 + 1 + 1 + i * 65 + 1 + j;
-                            Memory.Write(pointer + offset * 8, slotMachineSlot);
-                        }
-                    }
-                }
-            }
-
-            Thread.Sleep(250);
+            Thread.Sleep(20);
         }
     }
 
@@ -80,9 +60,24 @@ public partial class CasinoHackWindow
     {
         AudioHelper.PlayClickSound();
 
-        slotMachineSlot = ListBox_SlotMachine.SelectedIndex;
+        var slotMachineSlot = ListBox_SlotMachine.SelectedIndex;
 
         if (ListBox_SlotMachine.SelectedItem is ListBoxItem item)
             TextBlock_SlotMachineValue.Text = $"奖品：{item.Content}";
+
+        var pointer = Locals.LocalAddress("casino_slots");
+        if (Memory.IsValid(pointer))
+        {
+            pointer = Memory.Read<long>(pointer);
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 64; j++)
+                {
+                    var offset = 1344 + 1 + 1 + i * 65 + 1 + j;
+                    Memory.Write(pointer + offset * 8, slotMachineSlot);
+                }
+            }
+        }
     }
 }
